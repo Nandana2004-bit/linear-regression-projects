8193b48503d305fca4e29fef1b82c2c8ae4a71aa
Nandana2004-bit <nandanajithesh2004@gmail.com>
2025-12-17 22:42:26 +0530

    battery life prediction

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn .model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 
-df = pd.read_csv("house_rent.csv")
-#print(df.head())
+df = pd.read_csv(" battery_life.csv")
+#print(df)
 
-#02
-df.shape
-numeric_cols_df = df.select_dtypes(include=np.number)
-#print(numeric_cols_df.columns)
+a = df.select_dtypes(include='number')
+b = df.select_dtypes(include='object')
+#print(a)
+#print(b)
+df.info()
 df.describe()
 
-#03
+
 #print(df.isnull())
-df["age"].fillna(df["age"].mean(), inplace=True)
-#print(df.isnull())
-#print(df.duplicated())
+df["network"]
+c = {'WiFi': 0, 'MobileData' : 1}
+df['encoded network'] = df['network'].map(c)
+#print(df['network_mapped'])
 
-#04
-X = df[["size_sqft", "bedrooms", "age"]]
-Y = df[["rent"]]
+X = df[["screen_time", "apps_used", "encoded network"]]
+y = df[["battery_hours"]]
 
-#05
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
-
-#06
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 model = LinearRegression()
-model.fit(X_train,Y_train)
+model.fit(X_train, y_train)
 
-#07
 y_pred = model.predict(X_test)
 print(y_pred)
-
-
